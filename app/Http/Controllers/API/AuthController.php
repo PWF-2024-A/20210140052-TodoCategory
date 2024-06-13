3e57a2a260f559aaff7a8103e9a03a0568303d7c
Khawarizmi Azis Septian <rizmiseptian@gmail.com>
2024-06-13 14:10:07 +0700

    api : create, update, delete data

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -7,33 +7,32 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
-{
-    $credentials = $request->only('email', 'password');
-    if (auth()->attempt($credentials)) {
-        $user = $request->user();
-        if ($user->tokens) {
-            $user->tokens()->delete();
-        }
-        $token = $user->createToken('Personal Access Token')->plainTextToken;
+    public function login (Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        if (auth()->attempt($credentials)) {
+            $user = $request->user();
+            if ($user->tokens) {
+                $user->tokens()->delete();
+            }
+        $token = $user->createToken ('Personal Access Token')->plainTextToken;
         return response()->json([
             'status' => 'Login success!',
             'token_type' => 'Bearer',
             'access_token' => $token
         ], 201);
-    } else {
-        return response()->json([
-            'status' => 'Login fail!'
-        ], 401);
+        } else {
+                return response()->json([
+                    'status' => 'Login fail!'
+                ], 401);
+            }
+        }
+        public function logout (Request $request)
+        {
+            $request->user()->tokens()->delete();
+            return response()->json([
+                'status' => 'Logout success!'
+            ], 200);
     }
-}
-
-public function logout(Request $request)
-{
-    $request->user()->tokens()->delete();
-    return response()->json([
-        'status' => 'Logout success!'
-    ], 200);
-}
 
 }
---
+++ tugas12.md
@@ -0,0 +1,25 @@
+# Tugas 12
+
+## 1. Login
+
+![alt text](<screenshot/tugas12/Cuplikan layar 2024-05-29 153423.png>)
+
+## 2. Category Index
+
+![alt text](<screenshot/tugas12/Cuplikan layar 2024-06-13 134310.png>)
+
+## 3. Todo Store
+
+![alt text](<screenshot/tugas12/Cuplikan layar 2024-06-13 134310.png>)
+![alt text](<screenshot/tugas12/Cuplikan layar 2024-06-13 134927.png>)
+
+## 4. Todo Update
+
+![alt text](<screenshot/tugas12/Cuplikan layar 2024-06-13 135445.png>)
+![alt text](<screenshot/tugas12/Cuplikan layar 2024-06-13 135824.png>)
+![alt text](<screenshot/tugas12/Cuplikan layar 2024-06-13 140048.png>)
+
+## 5. Todo Delete
+
+![alt text](<screenshot/tugas12/Cuplikan layar 2024-06-13 140308.png>)
+![alt text](<screenshot/tugas12/Cuplikan layar 2024-06-13 140448.png>)
